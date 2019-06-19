3af1eae79bc227cc10032a739b7b9ccbd544c7a2
wf000017 <a159734682@gmail.com>
2019-06-20 01:28:04 +0800

    66666666666

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="jifu-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="index.html"
+          target="_self"
           rel="noopener noreferrer"
         >
-          Learn React
+        <p>
+          京富開發建設有限公司
+        </p>
         </a>
       </header>
     </div>
